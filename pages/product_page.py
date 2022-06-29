65757c613d07e784fdd248401e34a1d475905f2f
KAA-sign <q89308033587@gmail.com>
2022-06-29 11:25:10 +0300

    looking for a bug

--- pages/product_page.py
+++ pages/product_page.py
@@ -10,12 +10,11 @@ class ProductPage(BasePage):
         basket_button.click()
 
     def check_price_and_nane_product_in_basket(self):
-        # time.sleep(60)
         product_name = self.browser.find_element(*PrductPageLocators.PRODUCT_NAME).text
         product_price = self.browser.find_element(*PrductPageLocators.PRODUCT_PRISE).text
         add_to_basket_answer = self.browser.find_element(*PrductPageLocators.CONFIRM_ADD_TO_BASKET).text
         product_price_in_basket = self.browser.find_element(*PrductPageLocators.PRODUCT_PRISE_IN_BASKET).text
-        assert product_name in add_to_basket_answer, "Product not in basket"
+        assert product_name == add_to_basket_answer, "Product not in basket"
         assert product_price in product_price_in_basket, "Product price and basket price are not equal"
 
     
--- test_product_page.py
+++ test_product_page.py
@@ -1,13 +1,26 @@
+import pytest
 from .pages.product_page import ProductPage
 import time
 
 
-def test_guest_can_add_product_to_basket(browser):
-    link = "http://selenium1py.pythonanywhere.com/catalogue/the-shellcoders-handbook_209/?promo=newYear"
+@pytest.mark.parametrize('link', ["http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer0",
+                                  "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer1",
+                                  "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer2",
+                                  "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer3",
+                                  "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer4",
+                                  "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer5",
+                                  "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer6",
+                                  "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer7",
+                                  "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer8",
+                                  "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer9"])
+
+@pytest.mark.xfail(reason="incorrect parameter promo")
+def test_guest_can_add_product_to_basket(browser, link):
+    # link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=newYear2019"
     page = ProductPage(browser, link)   # инициализируем Page Object, передаем в конструктор экземпляр драйвера и url адрес 
     page.open()                      # открываем страницу
     page.add_to_basket()
     page.solve_quiz_and_get_code()
-    time.sleep(3)
+    # time.sleep(60)
     page.check_price_and_nane_product_in_basket()
     # time.sleep(60)
