d3de295559c5f505d395d6312140d88846816ca3
KAA-sign <q89308033587@gmail.com>
2022-07-01 15:19:18 +0300

    negative cheks in work

--- pages/product_page.py
+++ pages/product_page.py
@@ -1,22 +1,30 @@
 from .base_page import BasePage
-from .locators import PrductPageLocators
+from .locators import ProductPageLocators
 import time
 
 
 class ProductPage(BasePage):
 
     def add_to_basket(self):
-        basket_button = self.browser.find_element(*PrductPageLocators.ADD_TO_BASKET_BUTTON)
+        basket_button = self.browser.find_element(*ProductPageLocators.ADD_TO_BASKET_BUTTON)
         basket_button.click()
 
     def check_price_and_nane_product_in_basket(self):
-        product_name = self.browser.find_element(*PrductPageLocators.PRODUCT_NAME).text
-        product_price = self.browser.find_element(*PrductPageLocators.PRODUCT_PRISE).text
-        add_to_basket_answer = self.browser.find_element(*PrductPageLocators.CONFIRM_ADD_TO_BASKET).text
-        product_price_in_basket = self.browser.find_element(*PrductPageLocators.PRODUCT_PRISE_IN_BASKET).text
+        product_name = self.browser.find_element(*ProductPageLocators.PRODUCT_NAME).text
+        product_price = self.browser.find_element(*ProductPageLocators.PRODUCT_PRISE).text
+        add_to_basket_answer = self.browser.find_element(*ProductPageLocators.CONFIRM_ADD_TO_BASKET).text
+        product_price_in_basket = self.browser.find_element(*ProductPageLocators.PRODUCT_PRISE_IN_BASKET).text
         assert product_name == add_to_basket_answer, "Product not in basket"
         assert product_price in product_price_in_basket, "Product price and basket price are not equal"
+        
 
+    def should_not_be_success_message(self):
+        assert self.is_not_element_present(*ProductPageLocators.SUCCESS_MESSAGE), \
+            "Success message is presented, but should not be"
+
+    def should_dissapear_of_success_message(self):
+        assert self.find_element(*ProductPageLocators.SUCCESS_MESSAGE), \
+            "Success message is presented, but should not be"
     
 
     
--- test_product_page.py
+++ test_product_page.py
@@ -1,26 +1,43 @@
 import pytest
 from .pages.product_page import ProductPage
+from .pages.locators import ProductPageLocators
+
 import time
 
 
-@pytest.mark.parametrize('link', ["http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer0",
-                                  "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer1",
-                                  "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer2",
-                                  "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer3",
-                                  "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer4",
-                                  "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer5",
-                                  "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer6",
-                                  "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer7",
-                                  "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer8",
-                                  "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer9"])
+@pytest.mark.parametrize('link', ["http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"])
 
-@pytest.mark.xfail(reason="incorrect parameter promo")
-def test_guest_can_add_product_to_basket(browser, link):
-    # link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=newYear2019"
-    page = ProductPage(browser, link)   # инициализируем Page Object, передаем в конструктор экземпляр драйвера и url адрес 
-    page.open()                      # открываем страницу
+# @pytest.mark.xfail(reason="incorrect parameter promo")
+# def test_guest_can_add_product_to_basket(browser, link):
+#     page = ProductPage(browser, link)   # инициализируем Page Object, передаем в конструктор экземпляр драйвера и url адрес 
+#     page.open()                      # открываем страницу
+#     page.add_to_basket()
+#     # time.sleep(60)
+#     page.solve_quiz_and_get_code()
+#     page.check_price_and_nane_product_in_basket()
+    
+
+# @pytest.mark.xfail()
+def test_guest_cant_see_success_message_after_adding_product_to_basket(browser, link):
+    page = ProductPage(browser, link)
+    page.open()
     page.add_to_basket()
-    page.solve_quiz_and_get_code()
+    # page.solve_quiz_and_get_code()
     # time.sleep(60)
-    page.check_price_and_nane_product_in_basket()
-    # time.sleep(60)
+    page.is_not_element_present(*ProductPageLocators.SUCCESS_MESSAGE)
+    
+
+# @pytest.mark.xfail()
+def test_guest_cant_see_success_message(browser, link):
+    page = ProductPage(browser, link)
+    page.open()
+    # page.solve_quiz_and_get_code()
+    page.is_not_element_present(*ProductPageLocators.SUCCESS_MESSAGE)
+
+# @pytest.mark.xfail()
+def test_message_disappeared_after_adding_product_to_basket(browser, link):  
+    page = ProductPage(browser, link)
+    page.open()
+    page.add_to_basket()
+    page.is_disappeared(*ProductPageLocators.SUCCESS_MESSAGE)
+
